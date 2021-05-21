73ddf928f8261d9e2134a3e0673f25b28d15fb57
Husain Korasawala <husainkorasawala@gmail.com>
2021-05-21 20:18:52 +0530

    day-1-done

---
+++ src/components/Header.js
@@ -0,0 +1,74 @@
+import Image from "next/image";
+import {
+  MenuIcon,
+  ShoppingCartIcon,
+  SearchIcon,
+} from "@heroicons/react/outline";
+
+const Header = () => {
+  return (
+    <header>
+      {/* Top Nav */}
+      <div className="flex items-center bg-amazon_blue p-1 py-2 flex-grow">
+        {/* Amazon Icon */}
+        <div className="mt-2 flex flex-grow items-center sm:flex-grow-0">
+          <Image
+            src="https://links.papareact.com/f90"
+            height={40}
+            width={150}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
+
+        {/* Search Bar */}
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none"
+            type="text"
+          />
+          <SearchIcon className="h-12 p-4" />
+        </div>
+
+        {/* Right */}
+        <div className="text-white flex items-center space-x-6 mx-6 text-xs whitespace-nowrap">
+          <div className="link">
+            <p>Hello Husain Korasawala</p>
+            <p className="font-extrabold md:text-sm">Accounts & Lists</p>
+          </div>
+          <div className="link">
+            <p>Returns</p>
+            <p className="font-extrabold lg:text-sm">& Orders</p>
+          </div>
+          <div className="link flex relative items-center">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 text-center bg-yellow-400 rounded-full text-black font-bold">
+              0
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="hidden lg:inline font-extrabold md:text-sm mt-2">
+              Basket
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Bottum Nav */}
+      <div className="flex items-center space-x-4 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="link hidden xl:inline-flex">Electronics</p>
+        <p className="link hidden xl:inline-flex">Food & Grocery</p>
+        <p className="link hidden xl:inline-flex">Prime</p>
+        <p className="link hidden xl:inline-flex">Buy Again</p>
+        <p className="link hidden xl:inline-flex">Shopper's Toolkit</p>
+        <p className="link hidden xl:inline-flex">Health & Personal Care</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/ProductFeed.js
@@ -0,0 +1,56 @@
+import Product from "./Product";
+
+const ProductFeed = ({ products }) => {
+  return (
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+      {products
+        .slice(0, 4)
+        .map(({ id, title, price, discription, image, category }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            discription={discription}
+            image={image}
+            price={price}
+            category={category}
+          />
+        ))}
+      <img
+        className="md:col-span-full"
+        src="https://links.papareact.com/dyz"
+        alt=""
+      />
+      <div className="md:col-span-2">
+        {products
+          .slice(4, 5)
+          .map(({ id, title, price, discription, image, category }) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              discription={discription}
+              image={image}
+              price={price}
+              category={category}
+            />
+          ))}
+      </div>
+      {products
+        .slice(5, products.length)
+        .map(({ id, title, price, discription, image, category }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            discription={discription}
+            image={image}
+            price={price}
+            category={category}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default ProductFeed;
