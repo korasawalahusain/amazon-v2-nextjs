3bd6602ac56e11f5381734249ace26ae49cda3e2
Husain Korasawala <husainkorasawala@gmail.com>
2021-05-24 01:40:12 +0530

    removed-urls

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -23,10 +23,8 @@ export default async (req, res) => {
     },
     line_items: transformedItems,
     mode: "payment",
-    success_url:
-      "https://amazon-clone-nextjs-4i8n0na4z-husainkorasawala.vercel.app/success",
-    cancel_url:
-      "https://amazon-clone-nextjs-4i8n0na4z-husainkorasawala.vercel.app/checkout",
+    success_url: `${process.env.HOST}/success`,
+    cancel_url: `${process.env.HOST}/checkout`,
     metadata: {
       email,
       images: JSON.stringify(items.map((item) => item.image)),
